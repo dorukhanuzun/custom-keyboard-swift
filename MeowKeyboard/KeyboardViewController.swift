444314f71d4bf0f985331078a17d425ab51bd190
Dorukhan Uzun <dorukhanuzun@MacBook-Pro-2.local>
2020-10-23 08:12:12 -0400

    Swift custom keyboard example

---
+++ MeowKeyboard/KeyboardViewController.swift
@@ -0,0 +1,72 @@
+//
+//  KeyboardViewController.swift
+//  MeowKeyboard
+//
+//  Created by Dorukhan Uzun on 2020-10-22.
+//
+
+import UIKit
+
+class KeyboardViewController: UIInputViewController {
+
+    @IBOutlet var nextKeyboardButton: UIButton!
+    
+    override func updateViewConstraints() {
+        super.updateViewConstraints()
+        
+        // Add custom view sizing constraints here
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //MeowKeyboard
+        let meowButton = UIButton(type: UIButton.ButtonType.system)
+        meowButton.frame = CGRect(x: 150, y: 50, width: 150, height: 120)
+        meowButton.setBackgroundImage(UIImage(named: "meow.jpeg"), for: UIControl.State.normal)
+        meowButton.addTarget(self, action: #selector(meow), for: UIControl.Event.touchUpInside)
+        view.addSubview(meowButton)
+        
+        // Perform custom UI setup here
+        self.nextKeyboardButton = UIButton(type: .system)
+        
+        self.nextKeyboardButton.setTitle(NSLocalizedString("Next Keyboard", comment: "Title for 'Next Keyboard' button"), for: [])
+        self.nextKeyboardButton.sizeToFit()
+        self.nextKeyboardButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.nextKeyboardButton.addTarget(self, action: #selector(handleInputModeList(from:with:)), for: .allTouchEvents)
+        
+        self.view.addSubview(self.nextKeyboardButton)
+        
+        self.nextKeyboardButton.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
+        self.nextKeyboardButton.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+    }
+    
+    @objc func meow(){
+        let textProxy = textDocumentProxy as UITextDocumentProxy
+        textProxy.insertText("Meow")
+    }
+    
+    override func viewWillLayoutSubviews() {
+        self.nextKeyboardButton.isHidden = !self.needsInputModeSwitchKey
+        super.viewWillLayoutSubviews()
+    }
+    
+    override func textWillChange(_ textInput: UITextInput?) {
+        // The app is about to change the document's contents. Perform any preparation here.
+    }
+    
+    override func textDidChange(_ textInput: UITextInput?) {
+        // The app has just changed the document's contents, the document context has been updated.
+        
+        var textColor: UIColor
+        let proxy = self.textDocumentProxy
+        if proxy.keyboardAppearance == UIKeyboardAppearance.dark {
+            textColor = UIColor.white
+        } else {
+            textColor = UIColor.black
+        }
+        self.nextKeyboardButton.setTitleColor(textColor, for: [])
+    }
+
+}
